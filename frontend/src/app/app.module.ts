c34c1430f6e03371057ec3bb87e6f690c8c31e4b
Kavindu Chethani <46352484+kavindu-chethani@users.noreply.github.com>
2022-11-20 13:50:00 +0530

    frontend implementation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+//Create constant router for routing to book component
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BookComponent,
+    data: { title: 'Book List' }
+  },
+  { path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  }
+]; 
 
 @NgModule({
   declarations: [
     AppComponent,
     BookComponent
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
+  
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/book/book.component.ts
+++ frontend/src/app/book/book.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
 
+
+export class BookComponent {
+  books: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('/book').subscribe(data => {
+      this.books = data;
+    });
+  }
 }

973f6a276401953c63841d29772e49478eb43360
Kavindu Chethani <46352484+kavindu-chethani@users.noreply.github.com>
2022-11-20 11:45:47 +0530

    commit

---
+++ backend/models/Book.js
@@ -0,0 +1,39 @@
+const { text } = require('express');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new mongoose.Schema(
+    {
+        isbn:{
+            type: String,
+            required: true,
+        },
+        title:{
+            type: String,
+            required: true,
+        },
+        author:{
+            type: String,
+            required: true,
+        },
+        description:{
+            type: String,
+            required: true,
+        },
+        published_year:{
+            type: String,
+            required: true,
+        },
+        publisher:{
+            type: String,
+            required: true,
+        },
+        updated_date:{
+            type: String,
+            required: true,
+        },
+      
+
+    }
+);
+module.exports = mongoose.model('Book', BookSchema);
